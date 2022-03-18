4f5bd0e89e5cc928419b55a627eff79d7cfa9110
Hira <hirakumar@gmail.com>
2022-03-18 22:49:43 +0545

    fixed

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-import { ExampleComponent } from 'countdown'
+import { CountDown } from 'countdown'
 import 'countdown/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <CountDown
+      startdate='2022-03-20'
+      starttime='23:55:00'
+      url='https://images.freeimages.com/images/large-previews/d9c/parrot-attack-1410608.jpg'
+    />
+  )
 }
 
 export default App
